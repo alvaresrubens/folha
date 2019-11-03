37746e1306ae9155c9f13a4d3f29c8b6f62e0433
unknown <alvares.rubens@gmail.com>
2019-11-03 12:11:20 -0300

    Refresh update

---
+++ calculadora.js
@@ -0,0 +1,78 @@
+var valor1;
+var valor2;
+var operador;
+var resultado;
+
+
+
+function teclaNumerica(numero) {
+
+    if (resultado == null) {
+        document.getElementById("visor").value = document.getElementById("visor").value + numero;
+    } else {
+
+        document.getElementById("visor").value = document.getElementById("visor").value + numero;
+    }
+
+}
+
+function teclaFuncao(operacao) {
+
+    if (operador == null) {
+        valor1 = document.getElementById("visor").value;
+        operador = operacao;
+        document.getElementById("visorOperacao").value = valor1 + operador;
+        document.getElementById("visor").value = '';
+    } else {
+        valor1 = resultado;
+        operador = operacao;
+        document.getElementById("visorOperacao").value = valor1 + operador;
+        document.getElementById("visor").value = '';
+    }
+}
+
+function limpar() {
+    document.getElementById("visor").value = '';
+    document.getElementById("visorOperacao").value = '';
+    operador = null;
+    valor1 = null;
+    valor2 = null;
+    resultado = null;
+}
+
+function igual() {
+
+    if (operador == null && valor2 == null) {
+        document.getElementById("visor").value = document.getElementById("visor").value;
+    } else {
+        if (resultado != null) {
+            valor1 = resultado;
+            document.getElementById("visorOperacao").value = valor1 + operador + valor2;
+            resultado = calcular(operador, valor1, valor2);
+            document.getElementById("visor").value = resultado;
+        } else {
+            valor2 = document.getElementById("visor").value;
+            document.getElementById("visorOperacao").value = valor1 + operador + valor2;
+            resultado = calcular(operador, valor1, valor2);
+            document.getElementById("visor").value = resultado;
+        }
+    }
+}
+
+function calcular(operador, valor1, valor2) {
+
+    switch (operador) {
+        case "+":
+            final = parseFloat(valor1) + parseFloat(valor2);
+            return final;
+        case "-":
+            final = parseFloat(valor1) - parseFloat(valor2);
+            return final;
+        case "*":
+            final = parseFloat(valor1) * parseFloat(valor2);
+            return final;
+        case "/":
+            final = parseFloat(valor1) / parseFloat(valor2);
+            return final;
+    }
+}
